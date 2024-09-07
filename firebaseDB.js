7b1930a92804ad86d134d100e3a82855d4d20a4e
Suryatejaa <illasuryanani2001@gmail.com>
2024-09-07 17:38:27 +0530

    Google analytics installed

---
+++ firebaseDB.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2HRfN-hVJgtAZqAG-k2Ot5bTc0prvpVE",
+    authDomain: "calculator-surya.firebaseapp.com",
+    projectId: "calculator-surya",
+    storageBucket: "calculator-surya.appspot.com",
+    messagingSenderId: "397004330048",
+    appId: "1:397004330048:web:88e8d69e1446270fc0fba5",
+    measurementId: "G-FH817MH6R8",
+    databaseURL: "https://calculator-surya-default-rtdb.firebaseio.com/"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+
+
